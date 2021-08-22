f396bfb83e42b004143349053205a34722b13900
Craig Conway <craigconway@gmail.com>
2021-08-22 10:32:07 -0700

    add reader, cleanup crud

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
-import unicodedata
-import re
-import os.path
 import json
+import os.path
+import random
+import re
+import unicodedata
 
-import youtube_dl
 import vlc
+import youtube_dl
 
 from flask import Flask, render_template, redirect, request, url_for
 from flask_bootstrap import Bootstrap
@@ -14,8 +15,8 @@ from wtforms import StringField, SubmitField
 from wtforms.fields.simple import HiddenField
 from wtforms.validators import DataRequired
 
-# import RPi.GPIO as GPIO
-# from pn532 import *
+import RPi.GPIO as GPIO
+from pn532 import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'x3HqRBL-9GBo._VmJHY-'
@@ -23,16 +24,15 @@ Bootstrap(app)  # Flask-Bootstrap requires this line
 
 
 class TagForm(FlaskForm):
-    id = HiddenField('id', validators=[
-                     DataRequired()], default="DEFAULT")
+    id = HiddenField('id')
     name = StringField('Name', validators=[DataRequired()])
     link = StringField('YouTube Link', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Save')
 
 
 player = vlc.MediaListPlayer()
 playing = False
-reading = True
+reading = False
 data_file = "data.json"
 
 
@@ -64,7 +64,7 @@ def download(video_url):
     filename = slugify(video_url)
     print('Exists: ', os.path.isfile(filename))
     ydl_opts = {
-        'outtmpl': filename + '.mp3',
+        'outtmpl': '%s.%s' % (filename, '%(etx)s'),
         'nocheckcertificate': True,
         'format': 'bestaudio/best',
         'keepvideo': False,
@@ -78,38 +78,49 @@ def download(video_url):
         r = ydl.download([video_url])
 
 
-tag_map = get_data()["tags"]
+tags = get_data()["tags"]
 
-# try:
-#     pn532 = PN532_SPI(debug=False, reset=20, cs=4)
-#     ic, ver, rev, support = pn532.get_firmware_version()
-#     print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
-#     pn532.SAM_configuration()
-#     print('Waiting for RFID/NFC card...')
-#     while reading:
-#         uid = pn532.read_passive_target(timeout=0.5)
-#         if uid is None:
-#             player.stop()
-#             playing = False
-#             continue
-#         print('Found uid.hex():', uid.hex())
-#         if not playing:
-#             try:
-#                 print("Playing: ", tag_map[uid.hex()])
-#                 player.set_media_list(vlc.MediaList([slugify(tag_map[uid.hex()])]))
-#                 player.play()
-#                 playing = True
-#             except Exception as x:
-#                 print("No mapping: ", uid.hex())
-#                 # TODO play default
+try:
+    pn532 = PN532_SPI(debug=False, reset=20, cs=4)
+    ic, ver, rev, support = pn532.get_firmware_version()
+    print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+    pn532.SAM_configuration()
+    print('Waiting for RFID/NFC card...')
+    while reading:
+        uid = pn532.read_passive_target(timeout=0.5)
+        if uid is None:
+            player.stop()
+            playing = False
+            continue
+        print('Found uid.hex():', uid.hex())
+        if not playing:
+            try:
+                print("Playing: ", tags[uid.hex()]["link"])
+                player.set_media_list(vlc.MediaList(
+                    [slugify(tags[uid.hex()]["link"])+".mp3"]))
+                player.play()
+                playing = True
+            except Exception as x:
+                print("No mapping: ", uid.hex())
+                # TODO play default
 
-# except Exception as e:
-#     print(e)
+except Exception as e:
+    print(e)
+
+
+def get_tag_id():
+    print("Getting tag from reader...")
+    try:
+        uid = pn532.read_passive_target(timeout=0.5)
+        return uid.hex()
+    except Exception as e:
+        print(e)
+        return random.getrandbits(8)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', message=get_data()["tags"])
+    return render_template('index.html', tags=get_data()["tags"])
 
 
 @app.route('/edit', methods=['GET', 'POST'])
@@ -118,27 +129,24 @@ def edit():
     data = get_data()
     form = TagForm()
     tags = data["tags"]
-    if "tag" in args:
+    tag = ""
+    if "tag" in args and request.method == "GET":
         tag = args["tag"]
         form.id.data = tag
         form.name.data = tags[tag]["name"]
         form.link.data = tags[tag]["link"]
-    if form.validate_on_submit():
-        id = get_tag_id()
-        download(form.link.data)
-        data["tags"][id] = {
-            "name": form.name.data,
-            "link": form.link.data,
-        }
+    if "delete" in args:
+        tags.pop(tag, None)
+        data["tags"] = tags
         write_data(data)
-        return render_template('index.html', message=data["tags"])
-    return render_template('edit.html', form=form, message="")
-
-
-def get_tag_id():
-    try:
-        uid = pn532.read_passive_target(timeout=0.5)
-        return uid.hex()
-    except Exception as e:
-        print(e)
-        return "TAG_NOT_FOUND"
+        return redirect("/")
+    if form.validate_on_submit():
+        tag = form.id.data
+        if not tag:
+            tag = get_tag_id()
+        download(form.link.data)
+        tags[tag] = {"name": form.name.data, "link": form.link.data}
+        data["tags"] = tags
+        write_data(data)
+        return redirect("/")
+    return render_template('edit.html', form=form, tag=tag)
